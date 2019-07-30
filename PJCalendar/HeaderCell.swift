26e1df7315b406d52e14572026a7cbbad7285602
Nicolas Bellon <nbellon.externe@pagesjaunes.fr>
2019-07-30 14:52:08 +0200

    [Fix] label text shrink

--- PJCalendar/HeaderCell.swift
+++ PJCalendar/HeaderCell.swift
@@ -10,10 +10,14 @@ import Foundation
 import UIKit
 class HeaderCell: UICollectionReusableView {
 
+  override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+    super.apply(layoutAttributes)
+  }
+
   let label: UILabel = {
     let dest = UILabel(frame: .zero)
     dest.translatesAutoresizingMaskIntoConstraints = false
-    dest.numberOfLines = 1
+    dest.numberOfLines = 0
     return dest
   }()
 
@@ -26,18 +30,19 @@ class HeaderCell: UICollectionReusableView {
     label.text = "HEADER"
     label.font = UIFont.systemFont(ofSize: 48)
     label.adjustsFontSizeToFitWidth = true
-    label.minimumScaleFactor = 0.5
-    label.adjustsFontForContentSizeCategory = true
+    label.minimumScaleFactor = 0.1
     label.textColor = .red
     label.backgroundColor = UIColor.yellow
     label.lineBreakMode = .byClipping
+    label.clipsToBounds = true
+    label.textAlignment = .center
 
     var constraints = [NSLayoutConstraint]()
 
     constraints.append(self.label.topAnchor.constraint(equalTo: self.topAnchor))
     constraints.append(self.label.bottomAnchor.constraint(equalTo: self.bottomAnchor))
-    constraints.append(self.label.centerXAnchor.constraint(equalTo: self.centerXAnchor))
-
+    constraints.append(self.label.leadingAnchor.constraint(equalTo: self.leadingAnchor))
+    constraints.append(self.label.trailingAnchor.constraint(equalTo: self.trailingAnchor))
 
     NSLayoutConstraint.activate(constraints)
 
